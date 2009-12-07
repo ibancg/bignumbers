6c2cd73d1c698d3f3361d9b492562cc81277a9f1
Ibán Cereijo <ibancg@gmail.com>
2009-12-07 17:19:46 +0100

    Added README and license snippets. Removed some advanced features

--- bignum.h
+++ bignum.h
@@ -1,3 +1,21 @@
+/*
+ BigNumbers - Arbitrary precision arithmetic
+ Copyright 2000-2009, Ibán Cereijo Graña <ibancg at gmail dot com>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __BIGNUMBER_H_
 #define __BIGNUMBER_H_
 
@@ -56,25 +74,6 @@ public:
 	// A, B and C are overlappables
 	friend void mul(BigNumber& A, BigNumber& B, BigNumber& C);
 
-	// Computes the inverse of A, B = 1/A
-	friend void inv(BigNumber& A, BigNumber& B);
-
-	// Computes C = A/B
-	friend void div(BigNumber& A, BigNumber& B, BigNumber& C);
-
-	// Performs a shift left operation of n digits. The result will be B = A*10^n
-	// A and B are overlappables
-	friend void shl(BigNumber& A, BigNumber& B, int n);
-
-	// Performs a shift left operation of n digits. The result will be B = A/10^n
-	// A and B are overlappables
-	friend void shr(BigNumber& A, BigNumber& B, int n);
-
-	// Computes the square root
-	friend void sqrt(BigNumber& A, BigNumber& B);
-
-	// Computes the quartic root
-	friend void sqrt4(BigNumber& A, BigNumber& B);
 };
 
 #endif
--- config.h
+++ config.h
@@ -1,3 +1,20 @@
+/*
+ BigNumbers - Arbitrary precision arithmetic
+ Copyright 2000-2009, Ibán Cereijo Graña <ibancg at gmail dot com>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef __CONFIG_H_
 #define __CONFIG_H_
@@ -12,16 +29,12 @@
 // floating point precission
 #define flt_t double  
 
-#define N_DIGITS		2048  // number of digits in the format.
-#define N_FRAC_DIGITS	2000  // number of fractional digits.
-
+#define N_DIGITS		128  // number of digits in the format.
+#define N_FRAC_DIGITS	100  // number of fractional digits.
 typedef unsigned char bcd_t; // BCD digit type.
 
 // advanced configuration (algorithm switches).
 
 #define FFT_MUL_ALGORITHM
-#define INVERSE_DIV_ALGORITHM
-#define INVERSE_NEWTON_SQRT_ALGORITHM
-#define INVERSE_NEWTON_SQRT4_ALGORITHM
 
 #endif
